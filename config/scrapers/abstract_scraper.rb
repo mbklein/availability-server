95c84aab7530e7510aee89255adf3b37f001613b
Michael B. Klein <mbklein@gmail.com>
2010-04-16 09:55:56 -0700

    Updated API version

--- config/scrapers/abstract_scraper.rb
+++ config/scrapers/abstract_scraper.rb
@@ -4,7 +4,7 @@ require 'ostruct'
 
 class AvailabilityHash
 
-  API_VERSION = '0.1.5'
+  API_VERSION = '0.1.7'
   
   def initialize(attributes = {})
     @hash = { 'version' => API_VERSION, 'availabilityItems' => [] }.merge(attributes)
